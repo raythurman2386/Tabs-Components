09bc4670c21611b4ef599e44691c7dbddd0a79a0
Ray Thurman <raymondthurman5@gmail.com>
2019-05-09 08:45:13 -0400

    Tab item initially set up.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,7 +8,7 @@ class TabLink {
     // console.log(this.element.dataset.tab)
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.element.dataset.tab}']`);
-    console.log(this.itemElement);
+    // console.log(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.tabItem);
 
@@ -17,35 +17,40 @@ class TabLink {
   };
 
   select() {
-    console.log('clicked');
+    // console.log('clicked');
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
+    // console.log(links);
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach(link => {
+      link.classList.remove('tabs-link-selected')
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
 
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element = element;
   }
 
   select() {
+    console.log('clicked0');
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-
+    Array.from(items).forEach(item => {
+      item.classList.remove('tabs-link-selected')
+    });
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add('tabs-link-selected');
   }
 }
 
