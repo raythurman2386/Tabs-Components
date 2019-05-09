416a7b71635165a1bfe3d8b9116163f848fb2e96
Ray Thurman <raymondthurman5@gmail.com>
2019-05-09 08:58:37 -0400

    No errors but proper text is not showing up

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,21 +5,25 @@ class TabLink {
 
     // Get the custom data attribute on the Link
     this.element.dataset.tab;
+
     // console.log(this.element.dataset.tab)
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.element.dataset.tab}']`);
+
     // console.log(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new TabItem(this.tabItem);
+    this.tabItem = new TabItem(this.itemElement);
 
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => this.select());
   };
 
   select() {
+
     // console.log('clicked');
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
+
     // console.log(links);
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach(link => {
@@ -38,17 +42,20 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     this.element = element;
+    console.log(element)
   }
 
   select() {
-    console.log('clicked0');
+    console.log('clicked');
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');
+    console.log(items);
 
     // Remove the class "tabs-item-selected" from each element
     Array.from(items).forEach(item => {
       item.classList.remove('tabs-link-selected')
     });
+
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-link-selected');
   }
