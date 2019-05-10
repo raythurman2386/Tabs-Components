0041e40b8d0d08828aac2d66de3bbb8c5480c264
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-09 20:24:29 -0400

    dropdown animation with pure css

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -17,9 +17,7 @@ class Dropdown {
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
-    TweenMax.to($('.dropdown-content'), 1, {
-      autoAlpha: 1
-    });
+
   }
 }
 
